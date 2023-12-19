1932ae1944df6300b5711d0f968409181e210cc1
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2023-12-19 13:11:17 +0100

    Add files via upload

---
+++ ZadatakZrakoplov.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zrakoplov
+{
+    class Zrakoplov
+    {
+        private string naziv, snagaMotora, dosegLeta;
+
+        public void setnaziv(string naziv)
+        {
+            this.naziv = naziv;
+        }
+        public string getNaziv()
+        {
+            return this.naziv;
+        }
+        public void setSnagaMotora(string snagaMotora)
+        {
+            this.snagaMotora = snagaMotora;
+        }
+        public string getSnagaMotora()
+        {
+            return this.snagaMotora;
+        }
+        public void setDosegLeta(string dosegLeta)
+        {
+            this.dosegLeta = dosegLeta;
+        }
+        public string getDosegLeta()
+        {
+            return this.dosegLeta;
+        }
+        public override string ToString()
+        {
+            string ispis = "naziv: " + this.naziv
+                + "\nSnaga motora: " + this.snagaMotora
+                + "\nDoseg Leta: " + this.dosegLeta;
+            return ispis;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // instanciranje novog objekta
+            Zrakoplov zrakoplov = new Zrakoplov();
+
+            //korisnički unos
+            Console.WriteLine("Unesi naziv, snagu motora i doseg leta");
+            string naziv = Console.ReadLine();
+            string snagaMotora = Console.ReadLine();
+            string dosegLeta = Console.ReadLine();
+
+            // definiranje podataka za objekt 'ucenik'
+            zrakoplov.setnaziv(naziv);
+            zrakoplov.setSnagaMotora(snagaMotora);
+            zrakoplov.setDosegLeta(dosegLeta);
+
+            // ispis podataka
+            Console.WriteLine(zrakoplov.ToString());
+
+
+            Console.ReadKey();
+        }
+    }
+}
